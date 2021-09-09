bef686a83456b079088d25cf5fd5f71c8d0c6248
Daniel Gonzalez <daniel.gonzalez@codescience.com>
2021-09-09 13:05:58 -0400

    fix error

--- server.js
+++ server.js
@@ -13,26 +13,34 @@ app.use(bodyParser()); // pull information from html in POST
 app.use(express.static(__dirname + '/public'));
 
 app.post('/signedrequest', function(req, res) {
+    console.log('I got signedrequest', req.body.signed_request);
 
     // You could save this information in the user session if needed
     var signedRequest = decode(req.body.signed_request, consumerSecret),
-        context = signedRequest.context;//,
+        context = signedRequest.context,
         oauthToken = signedRequest.client.oauthToken,
-        instanceUrl = signedRequest.client.instanceUrl,
+        instanceUrl = signedRequest.client.instanceUrl;
 
-        this is not necessary but documented here for demo
-        query = "SELECT Id, FirstName, LastName, Phone, Email FROM Contact WHERE Id = '" + context.environment.record.Id + "'",
+    console.log('I decoded signedrequest', signedRequest);
 
-        contactRequest = {
-            url: instanceUrl + '/services/data/v52.0/query?q=' + query,
-            headers: {
-                'Authorization': 'OAuth ' + oauthToken
-            }
-        };
+    // this is not necessary but documented here for demo
+    var query = "SELECT Id, FirstName, LastName, Phone, Email FROM Contact WHERE Id = '" + context.environment.record.Id + "'";
+
+    var contactRequest = {
+        url: instanceUrl + '/services/data/v52.0/query?q=' + query,
+        headers: {
+            'Authorization': 'OAuth ' + oauthToken
+        }
+    };
 
     request(contactRequest, function(err, response, body) {
+        console.log('Contact from API response', response);
+
         var contact = context.environment.record,
             text = 'MECARD:N:' + contact.LastName + ',' + contact.FirstName + ';TEL:' + contact.Phone + ';EMAIL:' + contact.Email + ';;';
+
+        console.log('Contact from signed request context', contact);
+        
         QRCode.toDataURL(text, function(err, url) {
             res.render('index', {context: context, imgUrl: url, sr: JSON.stringify(signedRequest)});
         });
