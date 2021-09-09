b29d089a2d2ba83ec9a74d445ef8aec07ce477f7
Daniel Gonzalez <daniel.gonzalez@codescience.com>
2021-09-09 12:46:13 -0400

    first import

---
+++ README.md
@@ -0,0 +1,5 @@
+[![Deploy](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy)
+
+## Salesforce Canvas Demo
+
+Change CONSUMER_KEY and CONSUMER_SECRET in app.json or in Heroku after deploying.
---
+++ server.js
@@ -0,0 +1,52 @@
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    // request = require('request'),
+    qrcode = require('qrcode-npm'),
+    decode = require('salesforce-signed-request'),
+
+    consumerSecret = process.env.CONSUMER_SECRET,
+
+    app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser()); // pull information from html in POST
+app.use(express.static(__dirname + '/public'));
+
+app.post('/signedrequest', function(req, res) {
+
+    // You could save this information in the user session if needed
+    var signedRequest = decode(req.body.signed_request, consumerSecret),
+        context = signedRequest.context;//,
+        oauthToken = signedRequest.client.oauthToken,
+        instanceUrl = signedRequest.client.instanceUrl,
+
+        this is not necessary but documented here for demo
+        query = "SELECT Id, FirstName, LastName, Phone, Email FROM Contact WHERE Id = '" + context.environment.record.Id + "'",
+
+        contactRequest = {
+            url: instanceUrl + '/services/data/v52.0/query?q=' + query,
+            headers: {
+                'Authorization': 'OAuth ' + oauthToken
+            }
+        };
+
+    request(contactRequest, function(err, response, body) {
+        var qr = qrcode.qrcode(4, 'L'),
+            contact = context.environment.record,
+            text = 'MECARD:N:' + contact.LastName + ',' + contact.FirstName + ';TEL:' + contact.Phone + ';EMAIL:' + contact.Email + ';;';
+        qr.addData(text);
+        qr.make();
+        var imgTag = qr.createImgTag(4);
+        res.render('index', {context: context, imgTag: imgTag, sr: JSON.stringify(signedRequest)});
+    });
+});
+
+app.get('/oauth', function(req, res) {
+    res.render('oauth');
+});
+
+app.set('port', process.env.PORT || 5000);
+
+app.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
+});
